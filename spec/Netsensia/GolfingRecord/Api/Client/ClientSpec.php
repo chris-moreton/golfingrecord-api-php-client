bf4f0708133c6b68d5df3d2ab6efee44c00d4072
vagrant <vagrant@homestead>
2017-02-24 20:45:19 +0000

    Update tests to send API KEY

--- spec/Netsensia/GolfingRecord/Api/Client/ClientSpec.php
+++ spec/Netsensia/GolfingRecord/Api/Client/ClientSpec.php
@@ -12,78 +12,12 @@ class ClientSpec extends ObjectBehavior
         $this->shouldHaveType('Netsensia\GolfingRecord\Api\Client\Client');
     }
     
-    function it_can_get_a_bearer_token_using_password_grant()
-    {
-        $this->passwordGrant(
-            config('USERNAME'),
-            config('PASSWORD'),
-            config('PASSWORD_GRANT_CLIENT_SECRET')
-        )->shouldBeAnObjectContainingKeyAndValue('token_type', 'Bearer');
-    }
-    
-    function it_can_get_user_details_from_an_email_address()
-    {
-        $this->getUserDetails('chris@chrismo.com')->shouldBeAnObjectContainingKeyAndValue('id', config('USER_ID'));
-    }
-
     function it_can_get_user_details_from_an_id()
     {
-        $this->getUserDetails(config('USER_ID'))->shouldBeAnObjectContainingKeyAndValue('email', 'chris@chrismo.com');
+        $this->beConstructedWith(config('API_KEY'));
+        $this->getUserDetails(config('USER_ID'))->shouldBeAnObjectContainingKeyAndValue('id', config('USER_ID'));
     }
 
-    function it_can_verify_a_user_password()
-    {
-        $this->verifyPassword('chris@chrismo.com', config('PASSWORD'))->shouldBeAnObjectContainingKeyAndValue('verified', true);
-    }
-    
-    function it_can_find_that_a_user_password_is_wrong()
-    {
-        $this->verifyPassword('chris@chrismo.com', 'qweqwe')->shouldBeAnObjectContainingKeyAndValue('verified', false);
-    }
-    
-    function it_can_update_a_user_remember_token()
-    {
-        $r = md5(rand(0, PHP_INT_MAX));
-        $this->updateUserDetails(config('USER_ID'), [
-            'remember_token' => $r,
-        ])->shouldBeAnObjectContainingKeyAndValue('remember_token', $r);
-    }
-    
-    function it_will_report_an_attempt_to_update_an_invalid_user_field()
-    {
-        $this->shouldThrow('GuzzleHttp\Exception\ClientException')->during('updateUserDetails', [config('USER_ID'), [
-            'remembers_token' => 123,
-        ]]);
-    }
-    
-    function it_will_report_an_attempt_to_update_an_invalid_user()
-    {
-        $this->shouldThrow('GuzzleHttp\Exception\ClientException')->during('updateUserDetails', [-1, [
-            'remember_token' => 123,
-        ]]);
-    }
-    
-    function it_can_create_a_new_user()
-    {
-        $time = time();
-        $username = 'User' . $time;
-        $this->createUser([
-            'name' => $username, 
-            'email' => $username . '@netsensia.com', 
-            'password' => 'Pass' . $username]
-        )->shouldBeAnObjectContainingKeyAndValue('name', $username);
-    }
-
-    function it_will_report_an_attempt_to_create_a_user_with_an_invalid_field()
-    {
-        $time = time();
-        $this->shouldThrow('GuzzleHttp\Exception\ClientException')->during('createUser', [[
-            'names' => 'User' . $time, 
-            'email' => $time . '@netsensia.com', 
-            'password' => 'Pass' . $time
-        ]]);
-    }
-    
     public function getMatchers()
     {
         return [
--- src/Netsensia/GolfingRecord/Api/Client/Client.php
+++ src/Netsensia/GolfingRecord/Api/Client/Client.php
@@ -14,37 +14,6 @@ class Client
     private $apiBaseUri = 'http://api.golfingrecord.local';
     
     /**
-    * oAuth2 Password Grant
-    * 
-    * /oauth/token
-    * 
-    * @return boolean|mixed
-    */
-    public function passwordGrant($username, $password, $clientSecret, $scope = '*')
-    {
-        $response = $this->client()->request('POST', $this->apiBaseUri . '/oauth/token', $this->opts([
-            'form_params' => [
-                'grant_type' => 'password',
-                'username' => $username,
-                'password' => $password,
-                'client_id' => 2,
-                'client_secret' => $clientSecret,
-                'scope' => $scope,
-            ],
-        ]));
-
-        if( $response->getStatusCode() != 200 ){
-            return $this->log($response, false);
-        }
-    
-        $jsonDecode = json_decode($response->getBody());
-    
-        $this->log($response, true);
-    
-        return $jsonDecode;
-    }
-    
-    /**
      * User
      * 
      * /user/{id}
@@ -56,87 +25,6 @@ class Client
     public function getUserDetails($id)
     {
         $response = $this->client()->request('GET', $this->apiBaseUri . '/v1/users/' . $id, $this->opts());
-    
-        if( $response->getStatusCode() != 200 ){
-            return $this->log($response, false);
-        }
-    
-        $jsonDecode = json_decode($response->getBody());
-    
-        $this->log($response, true);
-    
-        return $jsonDecode;
-    }
-
-    /**
-     * Verify password
-     * 
-     * @param string $email
-     * @param string $password
-     * 
-     * @return boolean|mixed
-     */
-    public function verifyPassword($email, $password)
-    {
-        $response = $this->client()->request('POST', $this->apiBaseUri . '/v1/users/' . $email . '/passwordcheck', $this->opts([
-            'json' => [
-                'password' => $password,
-            ],
-        ]));
-        
-        if( $response->getStatusCode() != 200 ){
-            return $this->log($response, false);
-        }
-        
-        $jsonDecode = json_decode($response->getBody());
-        
-        $this->log($response, true);
-        
-        return $jsonDecode;
-    }
-    
-    /**
-     * Update user details
-     *
-     * @param string $id
-     * @param array $details
-     *
-     * @return boolean|mixed
-     */
-    public function updateUserDetails($id, array $details)
-    {
-        $options = $this->opts([
-            'json' => $details,
-        ]);
-        
-        $response = $this->client()->request('PUT', $this->apiBaseUri . '/v1/users/' . $id, $options);
-        
-        if( $response->getStatusCode() != 200 ){
-            return $this->log($response, false);
-        }
-    
-        $jsonDecode = json_decode($response->getBody());
-        
-        $this->log($response, true);
-    
-        return $jsonDecode;
-    }
-    
-    /**
-     * Create user
-     *
-     * @param string $id
-     * @param array $details [email|name|password]
-     *
-     * @return boolean|mixed
-     */
-    public function createUser(array $details)
-    {
-        $options = $this->opts([
-            'json' => $details,
-        ]);
-    
-        $response = $this->client()->request('POST', $this->apiBaseUri . '/v1/users', $options);
     
         if( $response->getStatusCode() != 200 ){
             return $this->log($response, false);
