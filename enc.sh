a4e25be1aa5bf819e5c0f6241ce794cb01514d52
Chris Moreton <chris@netsensia.com>
2017-04-08 16:43:57 +0200

    Update Travis variables

--- enc.sh
+++ enc.sh
@@ -1,6 +1,6 @@
 # Add the environment variables in .env as encrypted .travis.yml variables
 
-for OUTPUT in $(cat spec/.env)
+for OUTPUT in $(cat spec/.env.example)
 do
 echo Encrypting $OUTPUT
 travis encrypt $OUTPUT -a
