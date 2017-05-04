28eadec323e18b82be6de123539357772edea124
Chris Moreton <chris@netsensia.com>
2017-05-04 17:45:39 +0100

    Fix tests for column name change

--- spec/SpecHelper.php
+++ spec/SpecHelper.php
@@ -2,6 +2,7 @@
 include 'vendor/autoload.php';
 
 use Dotenv\Dotenv;
+use GuzzleHttp\Client as GuzzleClient;
 
 if (file_exists(__DIR__ . '/.env')) {
     $dotenv = new Dotenv(__DIR__);
@@ -9,6 +10,11 @@ if (file_exists(__DIR__ . '/.env')) {
 }
 
 function config($key) {
+    
+    if ($key == 'API_USER_KEY') {
+        return getUserToken();
+    }
+    
     $value = getenv($key);
 
     if (!$value) {
@@ -26,9 +32,30 @@ function getCourseData($name) {
             'hole_number' => $i,
             'par' => rand(1,6),
             'stroke_index' => $i,
-            'yardage' => rand(1,999),
+            'distance' => rand(1,999),
         ];
     }
 
     return $data;
+}
+
+function getUserToken() {
+    $client = new GuzzleClient(['http_errors' => false]);
+    
+    $response = $client->request('POST', config('AUTH_SERVER') . '/oauth/token', [
+        'form_params' => [
+            'grant_type' => 'password',
+            'username' => config('USERNAME'),
+            'password' => config('PASSWORD'),
+            'client_id' => config('PASSWORD_GRANT_CLIENT_ID'),
+            'client_secret' => config('PASSWORD_GRANT_CLIENT_SECRET'),
+            'scope' => 'user',
+        ]], false);
+
+    if ($response->getStatusCode() == 200 ){
+        $jsonDecode = json_decode($response->getBody());
+        return $jsonDecode->access_token;
+    }
+    
+    return false;
 }
